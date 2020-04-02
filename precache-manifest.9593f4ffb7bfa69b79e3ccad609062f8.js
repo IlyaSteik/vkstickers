11c62d2a04e2521d9ac3bd3f0a4813ab73a58959
Admin <ilya.steik4@gmail.com>
2020-04-02 12:24:17 +0300

    Updates

--- precache-manifest.9593f4ffb7bfa69b79e3ccad609062f8.js
+++ precache-manifest.9593f4ffb7bfa69b79e3ccad609062f8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46551e674f04b35ab62c17dcdba450c5",
+    "revision": "354b279ad648d4f5865615d40aa58bbb",
     "url": "/vkstickers/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vkstickers/static/css/2.733ba2d0.chunk.css"
   },
   {
-    "revision": "c360f7f4d07fc91d40f7",
+    "revision": "7bd18a0453fcaf9c6220",
     "url": "/vkstickers/static/css/main.e677441c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vkstickers/static/js/2.b0a64460.chunk.js"
   },
   {
-    "revision": "c360f7f4d07fc91d40f7",
-    "url": "/vkstickers/static/js/main.705217a3.chunk.js"
+    "revision": "7bd18a0453fcaf9c6220",
+    "url": "/vkstickers/static/js/main.141bfdad.chunk.js"
   },
   {
     "revision": "99c773c3c59c4155db8c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vkstickers/precache-manifest.28238183c5dedb9e071aadc0cce2ce86.js"
+  "/vkstickers/precache-manifest.9593f4ffb7bfa69b79e3ccad609062f8.js"
 );
 
 self.addEventListener('message', (event) => {
