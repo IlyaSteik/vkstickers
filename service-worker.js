b5a1f376da26024ebeaec14ca2545941081d1adb
Admin <ilya.steik4@gmail.com>
2020-06-06 23:15:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vkstickers/precache-manifest.4923d9ea6a35bdb9fabd036d2f37204c.js"
+  "/vkstickers/precache-manifest.66abbfc0d2f85dab85dddb2ce41bf49e.js"
 );
 
 self.addEventListener('message', (event) => {
